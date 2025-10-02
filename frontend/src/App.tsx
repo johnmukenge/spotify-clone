a48d2fc4be9c4201ce2020711d685f5bd002e6e1
john.mukenge <john.mukenge@emeal.nttdata.com>
2025-10-02 15:37:53 +0200

    setting up others files

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-red-500 font-bold">Hello </h1>
+      <button className="bg-blue-500 text-white px-4 py-2 rounded">
+        Click Me
+      </button>
+    </>
+  );
+}
+
+export default App;
