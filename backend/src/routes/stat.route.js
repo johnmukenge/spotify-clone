6f2d06486079c423ee31011b57f25deda618b980
john mukenge <johnsonmukenge@gmail.com>
2025-10-03 10:27:45 +0200

    stat and user controller and router

--- backend/src/routes/stat.route.js
+++ backend/src/routes/stat.route.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
+import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
+import { getStats } from "../controller/stat.controller.js";
 
 const router = Router();
 
-router.get("/stats", (req, res) => {
-  // Logic to get all stats
-});
+router.get("/", protectRoute, requireAdmin, getStats);
 
 router.post("/stats", (req, res) => {
   // Logic to create a new stat
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
+import {
+  getAllUsers,
+} from "../controller/user.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("User route with GET method");
-});
+router.get("/", protectRoute, getAllUsers);
+// TODO: Get messages between two users
+//router.get("/:id", protestRoute, getUserById);
+//router.post("/", protestRoute, createUser);
+//router.put("/:id", protestRoute, updateUser);
+//router.delete("/:id", protestRoute, deleteUser);
 
 export default router;
